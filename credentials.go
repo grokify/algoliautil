013ffe0ccc73afeb2cb6e4425fadac247154eb8c
grokify <johncwang@gmail.com>
2022-04-24 07:09:24 -1000

    lint: golanci: fix

--- credentials.go
+++ credentials.go
@@ -10,33 +10,33 @@ import (
 
 // go get github.com/algolia/algoliasearch-client-go/v3@v3.14.0
 
-const ENV_VAR_APP_CREDENTIALS_JSON = "ALGOLIA_APP_CREDENTIALS_JSON"
+const EnvVarAppCredentialsJSON = "ALGOLIA_APP_CREDENTIALS_JSON"
 
 type Credentials struct {
-	ApplicationId    string `json:"applicationId"`
-	SearchOnlyApiKey string `json:"searchOnlyApiKey"`
-	AdminApiKey      string `json:"adminApiKey"`
-	AnalyticsApiKey  string `json:"analyticsApiKey"`
-	MonitoringApiKey string `json:"monitoringApiKey"`
+	ApplicationID    string `json:"applicationId"`
+	SearchOnlyAPIKey string `json:"searchOnlyApiKey"`
+	AdminAPIKey      string `json:"adminApiKey"`
+	AnalyticsAPIKey  string `json:"analyticsApiKey"`
+	MonitoringAPIKey string `json:"monitoringApiKey"`
 }
 
 func (c Credentials) TrimSpace() {
-	c.ApplicationId = strings.TrimSpace(c.ApplicationId)
-	c.SearchOnlyApiKey = strings.TrimSpace(c.SearchOnlyApiKey)
-	c.AdminApiKey = strings.TrimSpace(c.AdminApiKey)
-	c.AnalyticsApiKey = strings.TrimSpace(c.AnalyticsApiKey)
-	c.MonitoringApiKey = strings.TrimSpace(c.MonitoringApiKey)
+	c.ApplicationID = strings.TrimSpace(c.ApplicationID)
+	c.SearchOnlyAPIKey = strings.TrimSpace(c.SearchOnlyAPIKey)
+	c.AdminAPIKey = strings.TrimSpace(c.AdminAPIKey)
+	c.AnalyticsAPIKey = strings.TrimSpace(c.AnalyticsAPIKey)
+	c.MonitoringAPIKey = strings.TrimSpace(c.MonitoringAPIKey)
 }
 
 func NewClient(c Credentials) (*search.Client, error) {
 	c.TrimSpace()
-	if len(c.ApplicationId) == 0 {
+	if len(c.ApplicationID) == 0 {
 		return nil, errors.New("no Algolia applicationId")
 	}
-	if len(c.AdminApiKey) > 0 {
-		return search.NewClient(c.ApplicationId, c.AdminApiKey), nil
-	} else if len(c.SearchOnlyApiKey) > 0 {
-		return search.NewClient(c.ApplicationId, c.SearchOnlyApiKey), nil
+	if len(c.AdminAPIKey) > 0 {
+		return search.NewClient(c.ApplicationID, c.AdminAPIKey), nil
+	} else if len(c.SearchOnlyAPIKey) > 0 {
+		return search.NewClient(c.ApplicationID, c.SearchOnlyAPIKey), nil
 	}
 	return nil, errors.New("no Algolia Search or Admin API Key")
 }
